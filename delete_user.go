475d610c9b31cc027194788c5a94b6b1d76c59bf
Udam Liyanage <udam1998@gmail.com>
2020-01-02 14:42:53 +0530

    Delete operation

--- delete_user.go
+++ delete_user.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func deleteUser(c *gin.Context) {
-	c.String(200, "Delete User")
+	opts := options.Delete().SetCollation(&options.Collation{
+		Locale:    "en_US",
+		Strength:  1,
+		CaseLevel: false,
+	})
+	objID, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		panic(err)
+	}
+	filter := bson.D{{"_id", objID}}
+	_, err = db.Collection.DeleteOne(context.TODO(), filter, opts)
+	if err != nil {
+		panic(err)
+	}
+	c.JSON(404, nil)
 }
