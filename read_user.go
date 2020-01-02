9e4a194610a6c9e2ebddf8d79e3ee7ce009fc96d
Udam Liyanage <udam1998@gmail.com>
2020-01-02 13:43:26 +0530

    Read operation

--- read_user.go
+++ read_user.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func readUser(c *gin.Context) {
-	c.String(200, "Read User")
+	var user map[string]interface{}
+	objID, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		panic(err)
+	}
+	filter := bson.M{"_id": objID}
+	err = db.Collection.FindOne(context.TODO(), filter).Decode(&user)
+	if err != nil {
+		panic(err)
+	}
+	delete(user, "password")
+	c.JSON(200, user)
 }
