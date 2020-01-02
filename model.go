4188bc212fde7b40764c5f880d6916397a4e7b7b
Udam Liyanage <udam1998@gmail.com>
2020-01-02 11:38:03 +0530

    Set nullable fields on model

--- model.go
+++ model.go
@@ -1,16 +1,20 @@
 package main
 
-import "time"
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
 
 type User struct {
+	ID              primitive.ObjectID  `json:"_id,omitempty" bson:"_id,omitempty"`
 	FirstName       string              `json:"first_name" bson:"first_name"`
 	LastName        string              `json:"last_name" bson:"last_name"`
 	Email           string              `json:"email" bson:"email"`
 	Password        string              `json:"password" bson:"password"`
-	ContactNumbers  []string            `json:"contact_numbers" bson:"contact_numbers"`
-	Address         map[string]string   `json:"address" bson:"address"`
-	Devices         []map[string]string `json:"devices" bson:"devices"`
-	EmailVerifiedAt time.Time           `json:"email_verified_at" bson:"email_verified_at"`
-	CreatedAt       time.Time           `json:"created_at" bson:"created_at"`
-	UpdatedAt       time.Time           `json:"updated_at" bson:"updated_at"`
+	ContactNumbers  []string            `json:"contact_numbers,omitempty" bson:"contact_numbers,omitempty"`
+	Address         map[string]string   `json:"address,omitempty" bson:"address,omitempty"`
+	Devices         []map[string]string `json:"devices,omitempty" bson:"devices,omitempty"`
+	EmailVerifiedAt time.Time           `json:"email_verified_at,omitempty" bson:"email_verified_at,omitempty"`
+	CreatedAt       time.Time           `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt       time.Time           `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
