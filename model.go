4e4e63454c7c20b9f588663b19f2bc156e0f85b0
Udam Liyanage <udam1998@gmail.com>
2020-01-02 08:51:50 +0530

    User model V1

--- model.go
+++ model.go
@@ -1,1 +1,16 @@
 package main
+
+import "time"
+
+type User struct {
+	FirstName       string              `json:"first_name" bson:"first_name"`
+	LastName        string              `json:"last_name" bson:"last_name"`
+	Email           string              `json:"email" bson:"email"`
+	Password        string              `json:"password" bson:"password"`
+	ContactNumbers  []string            `json:"contact_numbers" bson:"contact_numbers"`
+	Address         map[string]string   `json:"address" bson:"address"`
+	Devices         []map[string]string `json:"devices" bson:"devices"`
+	EmailVerifiedAt time.Time           `json:"email_verified_at" bson:"email_verified_at"`
+	CreatedAt       time.Time           `json:"created_at" bson:"created_at"`
+	UpdatedAt       time.Time           `json:"updated_at" bson:"updated_at"`
+}
