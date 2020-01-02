b616bdb5d0fca4b88e95b6fe2f348ea41ed3d62f
Udam Liyanage <udam1998@gmail.com>
2020-01-02 09:18:21 +0530

    Global database connection pool

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "go.mongodb.org/mongo-driver/mongo"
+
+type Database struct {
+	Collection *mongo.Collection
+}
+
+var db = Database{Collection: connect()}
+
 func main() {
 	println("Main Function")
 }
