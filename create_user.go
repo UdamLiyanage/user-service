c1db08c3ee72132724920364fd9e93ce52f57e57
Udam Liyanage <udam1998@gmail.com>
2020-01-02 18:25:01 +0530

    Password hashing

--- create_user.go
+++ create_user.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"golang.org/x/crypto/bcrypt"
 	"time"
 )
 
@@ -12,9 +13,18 @@ func createUser(c *gin.Context) {
 	var user User
 	err := json.NewDecoder(c.Request.Body).Decode(&user)
 	checkError(c, err)
+	user.Password = hashPassword(user.Password)
 	user.CreatedAt = time.Now()
 	insertResult, err := db.Collection.InsertOne(context.TODO(), user)
 	checkError(c, err)
 	user.ID = insertResult.InsertedID.(primitive.ObjectID)
 	c.JSON(201, user)
+}
+
+func hashPassword(p string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(p), 10)
+	if err != nil {
+		panic(err)
+	}
+	return string(bytes)
 }
