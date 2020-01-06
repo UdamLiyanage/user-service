c2b4d4158ef31f516661d056032ce3785fb5a788
Udam Liyanage <udam1998@gmail.com>
2020-01-06 18:53:23 +0530

    Get attached organisation handler

--- user_organisation.go
+++ user_organisation.go
@@ -1,7 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 func getAttachedOrganisations(c *gin.Context) {
-	c.String(200, "Get attached organisation")
+	var res []map[string]interface{}
+	objID, err := primitive.ObjectIDFromHex(c.Param("id"))
+	checkError(c, err)
+	cursor, err := db.Collection.Find(
+		context.Background(),
+		bson.M{"_id": objID},
+		options.Find().SetProjection(bson.D{
+			{"organisations", 1},
+		}),
+	)
+	checkError(c, err)
+	for cursor.Next(context.TODO()) {
+		var user map[string]interface{}
+		err := cursor.Decode(&user)
+		checkError(c, err)
+		res = append(res, user)
+	}
+	c.JSON(200, res)
 }
