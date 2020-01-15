d4d343bfc8809c3cc639d832b4187046886cdfcc
Udam Liyanage <udam1998@gmail.com>
2020-01-15 13:31:33 +0530

    Changed test cases to fit new test document

--- read_user_test.go
+++ read_user_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestValidDeviceGet(t *testing.T) {
 	//Object ID - 5e0e2480031e0b729112ff2d is always available as a test document
-	testRequestStatusCode("GET", "/users/5e0e2480031e0b729112ff2d", nil, http.StatusOK, t)
+	testRequestStatusCode("GET", "/users/5e1ec13a22dbde30ad18aa50", nil, http.StatusOK, t)
 }
 
 func TestInvalidDeviceGet(t *testing.T) {
